0272f59e242d9b8f13f3f1a132e48341489da643
Mark Haube <markhaube@gmx.de>
2019-02-20 19:03:52 +0100

    Initial Commit.

--- src/data_api/main.go
+++ src/data_api/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/NullJupiter/GoTodoApp/src/data_api/db"
+	"github.com/NullJupiter/GoTodoApp/src/data_api/routing"
+)
+
+func main() {
+	// Initialize database instance
+	err := db.InitDB("postgres://localhost/todoApp?sslmode=disable")
+	if err != nil {
+		panic("Could not initialize a database instance!")
+	}
+
+	// Initialize routing and get router
+	router := routing.InitRouting()
+
+	// Listen and serve routes for router
+	http.ListenAndServe(":8080", router)
 }
