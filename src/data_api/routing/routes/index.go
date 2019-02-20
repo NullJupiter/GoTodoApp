75a58e0e37491c3682729206db01bf60f4ff349f
Mark Haube <markhaube@gmx.de>
2019-02-20 19:03:36 +0100

    Fixed naming.

--- src/data_api/routing/routes/index.go
+++ src/data_api/routing/routes/index.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-// IndexGetRouter function is used to handle GET requests on /
-func IndexGetRouter(w http.ResponseWriter, r *http.Request) {
+// IndexGetHandler function is used to handle GET requests on /
+func IndexGetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Go to /todos/{id} or /users/{id}"))
 }
