b9ec72dddda188a8f485854849e8363c991cbb10
Mark Haube <markhaube@gmx.de>
2019-02-20 20:56:34 +0100

    debugging...

--- src/data_api/db/queries/create_user.go
+++ src/data_api/db/queries/create_user.go
@@ -19,7 +19,7 @@ func CreateUserEntry(username string, passwordHash string) error {
 	fmt.Println(1)
 
 	// Create user entry in users table
-	_, err := db.GetDB().Exec("INSERT INTO users (username, passwordHash) VALUES ($1, $2);", username, passwordHash)
+	_, err := db.DB.Exec("INSERT INTO users (username, passwordHash) VALUES ($1, $2);", username, passwordHash)
 	if err != nil {
 		return err
 	}
@@ -33,7 +33,7 @@ func CreateUserEntry(username string, passwordHash string) error {
 
 	fmt.Println(3)
 
-	_, err = db.GetDB().Exec(fmt.Sprintf("CREATE TABLE user_%v (id SERIAL PRIMARY KEY, todo varchar(255) NOT NULL, done integer NOT NULL);", uid))
+	_, err = db.DB.Exec(fmt.Sprintf("CREATE TABLE user_%v (id SERIAL PRIMARY KEY, todo varchar(255) NOT NULL, done integer NOT NULL);", uid))
 	if err != nil {
 		return err
 	}
