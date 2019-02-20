022fe34088f3e86b2487584d9ed1d4722aaf9a7f
Mark Haube <markhaube@gmx.de>
2019-02-20 15:12:42 +0100

    CreateUserHandler function finished.

--- src/data_api/routing/routes/users.go
+++ src/data_api/routing/routes/users.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/NullJupiter/GoTodoApp/src/data_api/db/queries"
+)
 
 // OneUserGetHandler function is used to handle GET requests on /user/{id}
 func OneUserGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -9,5 +13,13 @@ func OneUserGetHandler(w http.ResponseWriter, r *http.Request) {
 
 // CreateUserHandler function is used to handle POST requests in /user/create
 func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	// Get username and passwordHash from POST data
+	username := r.FormValue("username")
+	passwordHash := r.FormValue("passwordHash")
 
+	err := queries.CreateUserEntry(username, passwordHash)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
