3e2d411e78c931bbf982d1ce8b06355487a8f790
Mark Haube <markhaube@gmx.de>
2019-02-20 19:48:36 +0100

    Fixed Syntax error.

--- src/data_api/routing/routes/index.go
+++ src/data_api/routing/routes/index.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 )
 
 // IndexGetHandler function is used to handle GET requests on /
 func IndexGetHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Go to /todos, /user/validate or /user/create")
+}
