dd2c833706aaec9bcd2d26bb4befb35dc9fc839a
Mark Haube <markhaube@gmx.de>
2019-02-20 20:53:07 +0100

    debugging...

--- src/data_api/db/queries/helper/getValues.go
+++ src/data_api/db/queries/helper/getValues.go
@@ -1,25 +1,22 @@
 package helper
 
 import (
-	"context"
 	"database/sql"
+	"fmt"
 
 	"github.com/NullJupiter/GoTodoApp/src/data_api/db"
 )
 
-var ctx = context.Background()
-
 // GetUIDForUname function is used to query the id for a specific username
-func GetUIDForUname(username string) string {
-	var uid string
-	var uname string
-	var pass string
-	err := db.GetDB().QueryRow("SELECT * FROM users WHERE username='NullJupiter;").Scan(&uid, &uname, &pass)
+func GetUIDForUname(username string) int {
+	var uid int
+	err := db.GetDB().QueryRow("SELECT id FROM users WHERE username=$1;", username).Scan(&uid)
+	fmt.Println(uid)
 	if err == sql.ErrNoRows {
-		return "0"
+		return 0
 	}
 	if err != nil {
-		return "0"
+		return 0
 	}
 
 	return uid
